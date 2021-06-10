01cd1bf55a576026148acc9c4144f21a103ff4e9
Adilzhan Ismailov <lekakz007@gmail.com>
2021-06-10 11:24:26 +0600

    refactoring

--- 9_Урок_Автоматическое_Тестирование_API/tests/_support/Step/Functional/createUpdateDelete.php
+++ 9_Урок_Автоматическое_Тестирование_API/tests/_support/Step/Functional/createUpdateDelete.php
@@ -19,19 +19,21 @@ class createUpdateDelete extends \FunctionalTester
         $I->seeResponseIsJson();
         $I->seeResponseContainsJson(['status' => 'ok']);
     }
+    // Создали userId чтобы поместить туда реузльтат urlToSend + Id - для переиспользования в других функциях
+    public $userId;
     /**
-     * Изменение пользователя = PUT, Удаление пользователя = DELETE
+     * Изменение пользователя = PUT
      */
-    public function updateAndDeleteUser(){
+    public function updateUser(){
         $I = $this;
          // Грабом вытащили ответ, преобразовали через decode и вытащили только _id
          $response = $I->grabResponse();
          $b = json_decode($response, true);
          $id = $b['_id'];
          $urlToSend = 'human?_id=';
-         $urlId = $urlToSend . $id;
+         $this->userId = $urlToSend . $id;
         // Отправляем новые данные
-         $I->sendPUT($urlId, ['name' => 'Adilzhan',
+         $I->sendPUT($this->userId, ['name' => 'Adilzhan',
                               'email' => 'adil.mids@gmail.com',
                               'owner' => 'Leka777kz',
                               'job'   => 'KolesaUpgrade',
@@ -41,10 +43,18 @@ class createUpdateDelete extends \FunctionalTester
         $I->seeResponseIsJson();
         $I->seeResponseContainsJson(['nModified' => '1']);
         $I->sendGet('people?owner=Leka777kz');
-        // Удаляем пользователя
-        $I->sendDelete($urlId);
+        
+    }
+    /**
+     *  Удаление пользователя = DELETE
+     */
+    public function deleteUser(){
+        $I = $this;
+        $I->sendDelete($this->userId);
         $I->seeResponseCodeIsSuccessful();
         $I->seeResponseIsJson();
         $I->seeResponseContainsJson(['deletedCount' => '1']);
+        $I->sendGet('people?owner=Leka777kz');
+        $I->dontSeeResponseContainsJson(['owner' => 'Leka777kz']);
     }
 }
